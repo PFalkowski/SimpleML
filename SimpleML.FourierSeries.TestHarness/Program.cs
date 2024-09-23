d92925ef87c0b75dcc26a80461a8be9c122b88bf
PFalkowski <piotr.falkowski.fm@gmail.com>
2024-09-24 01:59:04 +0200

    Refactor Fourier out

---
+++ SimpleML.FourierSeries.TestHarness/Program.cs
@@ -0,0 +1,27 @@
+﻿using SimpleML.AlphaVintageClient;
+using SimpleML.Configuration;
+using SimpleML.FourierSeries;
+
+
+public class Program
+{
+    public static async Task Main()
+    {
+        // Assume we have a method to load stock data
+        var config = new ConfigurationFactory().GetConfiguration();
+        var stockData = await new StockDataProvider(config).GetData();
+        var prices = stockData.Select(d => d.Close).ToList();
+
+        var predictor = new FourierSeriesPredictor(numCoefficients: 10);
+        predictor.Fit(prices);
+
+        var futureDays = 30;
+        var predictions = predictor.Predict(futureDays);
+
+        Console.WriteLine("Predictions for the next 30 days:");
+        for (var i = 0; i < predictions.Count; i++)
+        {
+            Console.WriteLine($"Day {i + 1}: {predictions[i]:C2}");
+        }
+    }
+}
--- SimpleML.FourierSeries/FourierSeriesPredictor.cs
+++ SimpleML.FourierSeries/FourierSeriesPredictor.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using MathNet.Numerics.IntegralTransforms;
-using Microsoft.Extensions.Configuration;
-using SimpleML.AlphaVintageClient;
-using SimpleML.Configuration;
+
+namespace SimpleML.FourierSeries;
 
 public class FourierSeriesPredictor
 {
@@ -24,10 +23,10 @@ public class FourierSeriesPredictor
         var normalizedPrices = prices.Select(p => p - _meanPrice).ToList();
 
         // Pad the input to a power of 2 for FFT efficiency
-        int n = 1;
+        var n = 1;
         while (n < normalizedPrices.Count) n *= 2;
         var paddedPrices = new Complex[n];
-        for (int i = 0; i < normalizedPrices.Count; i++)
+        for (var i = 0; i < normalizedPrices.Count; i++)
             paddedPrices[i] = new Complex(normalizedPrices[i], 0);
 
         // Perform FFT
@@ -40,43 +39,20 @@ public class FourierSeriesPredictor
     public List<double> Predict(int numDays)
     {
         var predictions = new List<double>();
-        int originalLength = _fourierCoefficients.Count;
+        var originalLength = _fourierCoefficients.Count;
 
-        for (int day = 0; day < numDays; day++)
+        for (var day = 0; day < numDays; day++)
         {
-            Complex sum = new Complex(0, 0);
-            for (int k = 0; k < _numCoefficients; k++)
+            var sum = new Complex(0, 0);
+            for (var k = 0; k < _numCoefficients; k++)
             {
-                double angle = 2 * Math.PI * k * (originalLength + day) / originalLength;
-                Complex exponential = new Complex(Math.Cos(angle), Math.Sin(angle));
+                var angle = 2 * Math.PI * k * (originalLength + day) / originalLength;
+                var exponential = new Complex(Math.Cos(angle), Math.Sin(angle));
                 sum += _fourierCoefficients[k] * exponential;
             }
             predictions.Add((sum / originalLength).Real + _meanPrice);
         }
 
         return predictions;
-    }
-}
-
-public class Program
-{
-    public static async Task Main()
-    {
-        // Assume we have a method to load stock data
-        var config = new ConfigurationFactory().GetConfiguration();
-        var stockData = await new StockDataProvider(config).GetData();
-        var prices = stockData.Select(d => d.Close).ToList();
-
-        var predictor = new FourierSeriesPredictor(numCoefficients: 10);
-        predictor.Fit(prices);
-
-        int futureDays = 30;
-        var predictions = predictor.Predict(futureDays);
-
-        Console.WriteLine("Predictions for the next 30 days:");
-        for (int i = 0; i < predictions.Count; i++)
-        {
-            Console.WriteLine($"Day {i + 1}: {predictions[i]:C2}");
-        }
     }
 }
